aa84ade510dded58a3785de5d84cb0781c134c2c
filipjt88 <filip88ks@gmail.com>
2025-03-20 21:04:20 +0100

    save

--- core/db.php
+++ core/db.php
@@ -1,5 +1,14 @@
 <?php
+$host = 'localhost';
+$dbname = 'guest_book';
+$username = 'root';
+$password = '';
 
-
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Nije povezana baza podataka!" . $e->getMessage());
+}
 
 ?>
