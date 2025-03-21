7171dff4d6c338f0904b640172cc56fbc5d859f7
filipjt88 <filip88ks@gmail.com>
2025-03-22 00:16:02 +0100

    save

--- actions/login.php
+++ actions/login.php
@@ -11,6 +11,12 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $email    = trim($_POST['email']);
     $password = trim($_POST['password']);
 
-    
+     if(!isset($_POST['email']) || empty($_POST['email'])) {
+        $username_error = "<p class='alert alert-danger text-danger d-inline-block p-1'>Email is required!</p>";
+        array_push($errors, $email_error);
+    } else {
+        $email = $_POST['email'];
+    }
+
 
 }
