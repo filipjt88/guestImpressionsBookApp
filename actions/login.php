37c41d5f81de9afdcf6ac3d10c3561b66b846125
filipjt88 <filip88ks@gmail.com>
2025-03-22 00:15:24 +0100

    save

--- actions/login.php
+++ actions/login.php
@@ -5,3 +5,12 @@ session_start();
 $_SESSION['errors'] = $errors;
 header("Location: ../views/register.view.php");
 exit;
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $email    = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    
+
+}
